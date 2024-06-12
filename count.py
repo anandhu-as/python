69f763e8130446c4519a9ab3d84c1b3982b5a759
anandhu-as <anandhuas010203@gmail.com>
2024-06-12 12:34:05 +0530

    .

--- count.py
+++ count.py
@@ -1,3 +1,13 @@
-num = 1000
-x = len(str(num))
-print(x)
+n = int(input("Enter a number: "))  5
+a = 0
+b = 1
+i = 0
+
+print("Fibonacci sequence:")
+while i < n: #0<5 
+    print(a, end=" ")#0 1
+    x = a + b # 1
+    a = b # 0 
+    b = x #  1
+    i =i+1 #
+print()  
