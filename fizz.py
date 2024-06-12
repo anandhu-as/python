930c54f8b88f928ce4120a0b81ba7040fec39dc0
anandhu-as <anandhuas010203@gmail.com>
2024-06-12 09:20:01 +0530

    updated

---
+++ fizz.py
@@ -0,0 +1,19 @@
+'''num=int(input("enter the number"))
+if num%5==0 and num% 3==0 :
+    print(f"{num} is  divisor of 5 and 3")
+else:
+      print(f"{num} is not divisor of 5 and 3") '''
+angle1=int(input("first"))
+angle2=int(input("second"))
+angle3=int(input("third"))
+deg=180
+sum=angle1 + angle2 + angle3
+if angle1 == 45 and angle2 == 45 and sum == deg:
+    print("It's a right-angled triangle")
+    print("It's a triangle")
+else:
+    print("It's not a triangle")
+
+ 
+
+
---
+++ main.py
@@ -0,0 +1,44 @@
+'''a=int(input("enter first   number"))
+b=int(input("enter second number"))
+c=int(input("enter third number"))
+
+if b > a and b> c: 
+  print( f"{b} is bigger number")
+elif a >b and a>c :
+   print( f"{a} is bigger number")
+else :
+  print( f"{c} is bigger number")'''
+
+year=int(input("enter a year"))
+if year %  4==0 and year %100 !=0 or year % 400 ==0:
+    print("leap year")
+else :
+    print("not leap year")
+
+
+
+
+#print(a==b)
+
+# a=100
+#a==100
+#print(a>b)
+#print(a<b)0
+'''
+num=int(input("enter a number"))
+if num==0:
+    print(num)
+elif num>0:
+    print("number is positive")
+else : 
+   print("number is negative")
+
+
+num=int(input("enter a number"))
+if num % 2 ==0:
+    print(f"{num} is even")
+else :
+    print(f"{num} + is oddd")'''
+
+
+
